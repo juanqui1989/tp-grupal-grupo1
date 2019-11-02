7400eda7c1d57ebdb4d178e08b3f1fcb8b701597
Facundo Ferrara <49033186+facuferrara@users.noreply.github.com>
2019-11-01 23:57:03 -0300

    Pruebas..

--- src/main/java/Tecnico.java
+++ src/main/java/Tecnico.java
@@ -1,4 +1,18 @@
 public class Tecnico {
 
-    
+    private PcBuilder pcBuilder;
+
+    public void setPcBuilder(PcBuilder pcBuilder) {
+        this.pcBuilder = pcBuilder;
+    }
+    public  void Computadora
+
+    public Comptadora getComputadora() {
+        return pcBuilder.getCompu();
+    }
+    public void construirPc(){
+        pcBuilder.buildMother();
+        pcBuilder.buildProcesador();
+        pcBuilder.buildMemoriaRam();
+    }
 }
